9259a781fdbb584c72650e4c827691ff0189cf0c
Nazia <nazia862@gmail.com>
2025-01-15 18:25:58 +0530

    "Daily Learning and Growing"

---
+++ src/test/java/RestAssured_API/BookingDates.java
@@ -0,0 +1,25 @@
+package RestAssured_API;
+
+public class BookingDates {
+
+    private String checkin;
+    private String checout;
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getChecout() {
+        return checout;
+    }
+
+    public void setChecout(String checout) {
+        this.checout = checout;
+    }
+
+
+}
